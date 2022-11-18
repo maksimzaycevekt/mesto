a54cf31ab11c464a61602230c9e3040a01b489ce
unknown <mazaitsev1996@yandex.ru>
2022-11-18 13:00:23 +0500

    fix: исправить комментари по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,38 @@
 let popup = document.querySelector('.popup');
 let buttonOpen = document.querySelector('.profile__button');
 let buttonClose = document.querySelector('.popup__close-button');
-let buttonSave = document.querySelector('popup__button');
 
-buttonOpen.addEventListener('click', () => {
+function popupOpen() {
   popup.classList.add('popup_opened');
-});
+  let nameInputValueT = document.getElementById('name').value;
+  let jobInputValueT = document.getElementById('job').value;
+  let names = document.querySelector('.profile__title').textContent;
+  let jobs = document.querySelector('.profile__subtitle').textContent;
+  nameInputValueT = names;
+  jobInputValueT = jobs;
+}
 
-buttonClose.addEventListener('click', () => {
+function popupClose() {
   popup.classList.remove('popup_opened');
-});
+}
 
-let formElement = document.querySelector('.popup__container');
+buttonOpen.addEventListener('click', popupOpen);
+
+buttonClose.addEventListener('click', popupClose);
+
+let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('name');
 let jobInput = document.querySelector('job');
 
 function formSubmitHandler (evt) {
-    evt.preventDefault('');
-    var nameInputValue = document.getElementById('name').value;
-    var jobInputValue = document.getElementById('job').value;
+    evt.preventDefault();
+    let nameInputValue = document.getElementById('name').value;
+    let jobInputValue = document.getElementById('job').value;
     let name = document.querySelector('.profile__title');
     let job = document.querySelector('.profile__subtitle');
     name.textContent = nameInputValue;
     job.textContent = jobInputValue;
-    popup.classList.remove('popup_opened');
+    popupClose();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
