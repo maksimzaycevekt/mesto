5bac0ed5fa43c4c99faa36a0dc0d040b2535e60c
unknown <mazaitsev1996@yandex.ru>
2022-11-19 16:38:56 +0500

    fix: внести корректировки по результатам ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,33 @@
 let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
 let buttonOpen = document.querySelector('.profile__button');
 let buttonClose = document.querySelector('.popup__close-button');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+
 
 function popupOpen() {
   popup.classList.add('popup_opened');
-  let nameInputValueT = document.getElementById('name').value;
-  let jobInputValueT = document.getElementById('job').value;
-  let names = document.querySelector('.profile__title').textContent;
-  let jobs = document.querySelector('.profile__subtitle').textContent;
-  nameInputValueT = names;
-  jobInputValueT = jobs;
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function popupClose() {
   popup.classList.remove('popup_opened');
 }
 
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popupClose();
+}
+
 buttonOpen.addEventListener('click', popupOpen);
 
 buttonClose.addEventListener('click', popupClose);
-
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('name');
-let jobInput = document.querySelector('job');
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    let nameInputValue = document.getElementById('name').value;
-    let jobInputValue = document.getElementById('job').value;
-    let name = document.querySelector('.profile__title');
-    let job = document.querySelector('.profile__subtitle');
-    name.textContent = nameInputValue;
-    job.textContent = jobInputValue;
-    popupClose();
-}
 
 formElement.addEventListener('submit', formSubmitHandler);
 
